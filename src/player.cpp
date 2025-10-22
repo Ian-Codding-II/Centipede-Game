c34e57c3b8e42dc8da4567909617fc4f18dea95d
Roman Salazar <Rasalazar@nic.edu>
2025-10-22 12:24:21 -0700

    player png added

--- src/player.cpp
+++ src/player.cpp
@@ -16,7 +16,7 @@ int main()
 
     sf::Texture playerTexture;
 
-    if(!playerTexture.loadFromFile("assets/tempPlayer.png", sf::IntRect({10, 10}, {32, 32}))) // Load temporary triangle PNG titled "tempPlayer.png"
+    if(!playerTexture.loadFromFile("assets/temporaryPlayer.png", sf::IntRect({10, 10}, {32, 32}))) // Load temporary triangle PNG titled "tempPlayer.png"
     {
         std::cerr << "There was an issue loading the player texture..." << '\n';
         return 0;
