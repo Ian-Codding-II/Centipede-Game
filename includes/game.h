8369e9da8f321d6b750b0df26652965e1cdce68c
Ian-Codding-II <icodding@nic.edu>
2025-11-22 17:04:26 -0800

    populated function headers

--- includes/game.h
+++ includes/game.h
@@ -12,25 +12,86 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <vector>
+#include "Game_State.h"
 #include "ScreenManager.h"
+#include "SettingsScreen.h"
+#include "Collision_Manager.h"
+#include "Centipede_Segment.h"
+#include "player.h"
+#include "bullet.h"
 
+// Forward declarations - these classes exist but we onl need pointers to them
+// This avoids circular includes
+class Player;
+class Centipede;
+class Bullet;
+class Mushroom;
+
+/**
+ * @class Game
+ * @brief Main game class that handels all gameplay logic
+ * 
+ */
 class Game {
-  public:
-    Game();
-    Game(sf::RenderWindow& win, ScreenManager& sm);
-    ~Game() {};
-    void handleInput();
-    void update(float time);
-    void render();
-    bool isDone() const;
 
   private:
-    sf::RenderWindow mWindow;
-    bool mIsDone;
-    sf::Texture mTextureTile;
-    sf::CircleShape mCherryPlant;
-    sf::Vector2f mIncrement;
-    void moveCherry(float time);
+  sf::RenderWindow & window;
+  ScreenManager& screenManager;
+
+  GameState currentState;
+  bool isGameOver;
+  bool isPaused;
+
+  int score;
+  int lives;
+  int level;
+
+  Player * player;
+  Centipede * centipede;
+  // If we ever make the spider class,
+  // then we would have a pointer to spider
+
+  std::vector<Bullet*> bullets;
+  std::vector<Mushroom*> mushrooms;
+
+  sf::Text scoreText; // Displays current score
+  sf::Text livesText; // Displays remaining lives
+  sf::Text levelText; // Displays current level
+
+  sf::RectangleShape background; // Black background rectangle
+
+  public:
+    Game();
+    Game(sf::RenderWindow& win, ScreenManager& screenMngr);
+    
+    ~Game();
+
+    void initualize();
+
+    void handleInput(const sf::Event& event);
+
+    void update(float dt);
+
+    void render();
+
+    GameState getState() const;
+
+    void cleanup();
+
+    bool checkCollision();
+
+    void addScore();
+
+    bool loseLife();
+
+    void completLevel();
+
+    bool damageCentipedeSegment(int segmentIndex);
+
+    void resetAfterDeath();
 };
 // - Initialize SFML window
 // - Manage game states (Menu, Playing, Paused, GameOver)
--- src/game.cpp
+++ src/game.cpp
@@ -1,110 +1,45 @@
 /**
  * @file game.cpp
  * @author Ian Codding II
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2025-10-21
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 #include "../includes/game.h"
+#include "../includes/errorHandler.h"
 #include "../includes/ScreenManager.h"
+#include <SFML/Graphics/RenderWindow.hpp>
 
 /**
  * @brief Construct a new Game:: Game object
- *
- */
-Game::Game()
-    : mWindow(sf::VideoMode(640, 480), "Bouncing sprite.") {
-    mWindow.setFramerateLimit(60);
-    mWindow.setPosition({50, 50});
-    mTextureTile.loadFromFile("assets/rpg_textures.png");
-    mCherryPlant.setRadius(31);
-    mCherryPlant.setTexture(&mTextureTile);
-    mCherryPlant.setTextureRect(sf::IntRect(64, 32, 62, 62));
-    mCherryPlant.setOrigin(62.f / 2.f, 62.f / 2.f);
-    mIncrement = sf::Vector2f(4.f, 4.f);
-    mIsDone = false;
-}
-
-
-Game::Game(sf::RenderWindow& win, ScreenManager& sm)
-{
-
-    
-
-
-
-
-}
-
-
-
-
-/**
- * @brief
- *
- */
-void Game::handleInput() {
-    sf::Event event;
-    while (mWindow.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            // Close window button clicked.
-            mWindow.close();
-        }
-    }
-}
-
-/**
- * @brief
- *
- */
-void Game::update(float time) {
-    moveCherry(time);
-}
-
-/**
- * @brief
- *
- */
-void Game::moveCherry(float time) {
-    sf::Vector2u winSize = mWindow.getSize();
-    int cherryX = 62;
-    int cherryY = 62;
-    float speed = 60; // controls speed
-
-    if ((mCherryPlant.getPosition().x + (cherryX / 2) > winSize.x && mIncrement.x > 0) ||
-        (mCherryPlant.getPosition().x - (cherryX / 2) < 0 && mIncrement.x < 0)) {
-        // Reverse the direction on X axis
-        mIncrement.x = -mIncrement.x;
-    }
-    if ((mCherryPlant.getPosition().y + (cherryY / 2) > winSize.y && mIncrement.y > 0) ||
-        (mCherryPlant.getPosition().y - (cherryY / 2) < 0 && mIncrement.y < 0)) {
-        // Reverse the direction on Y axis.
-        mIncrement.y = -mIncrement.y;
-    }
-    mCherryPlant.setPosition(
-        mCherryPlant.getPosition().x + mIncrement.x*speed*time,
-        mCherryPlant.getPosition().y + mIncrement.y*speed*time);
-}
-
-/**
- * @brief 
  * 
  */
-void Game::render() {
-    mWindow.clear(sf::Color::Black);
-    mWindow.draw(mCherryPlant);
-    mWindow.display();
+Game::Game(sf::RenderWindow& win, ScreenManager & screenMGR) 
+    :window(win),
+    screenManager(screenMGR),
+    currentState(GameState::MENU),
+    score(0),
+    lives(){
+    
 }
-
-/**
- * @brief
- *
- * @return true
- * @return false
- */
+Game::Game(sf::RenderWindow &win, ScreenManager &sm) {
+    
+}
+Game::~Game(){
+    
+}
+void Game::handleInput() {
+    
+}
+void Game::update(float time) {
+    
+}
+void Game::render() {
+    
+}
 bool Game::isDone() const {
-    return (!mWindow.isOpen() || mIsDone);
+    
 }
